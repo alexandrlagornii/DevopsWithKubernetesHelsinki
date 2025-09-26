8dcbebb265e62b1fd0f6715866ed998c62a44846
alago_0 <alago_0@proton.me>
2025-09-26 18:51:01 +0300

    3.10

--- theproject/TodoBackend/DbBackupCronJob/script.sh
+++ theproject/TodoBackend/DbBackupCronJob/script.sh
@@ -2,4 +2,4 @@ set -e
 URL="postgresql://${PG_USER}:${PG_PASS}@${PG_HOST}/${PG_DB}"
 DATE=$(date +%F)
 pg_dump -v "$URL" > /job/backup-$DATE.sql
-gsutil cp /job/backup.sql gs://alex-devopswithkubernetes-bucket/
+gsutil cp /job/backup-$DATE.sql gs://alex-devopswithkubernetes-bucket/
